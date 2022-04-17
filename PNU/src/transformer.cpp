aff7a9da5d4e17aaff43fa46f60c292c7655f918
ermakov-dmitry <dmitr.ermakov1@gmail.com>
2022-04-17 18:24:35 +0300

    Added new cases in tests and edited tabs.

--- PNU/src/transformer.cpp
+++ PNU/src/transformer.cpp
@@ -3,28 +3,28 @@
 
 namespace Transform {
     Spherical CartesianToPNU (const Cartesian& cartesian) {
-      Spherical ret;
-      ret.range = sqrt(pow(cartesian.x, 2.) + pow(cartesian.y, 2.) + pow(cartesian.z, 2.));
-      ret.theta = 270 - 180 * atan2(sqrt(pow(cartesian.z, 2.)
-                                         + pow(cartesian.x, 2.)), cartesian.y) / PI;
+        Spherical ret;
+        ret.range = sqrt(pow(cartesian.x, 2.) + pow(cartesian.y, 2.) + pow(cartesian.z, 2.));
+        ret.theta = 270 - 180 * atan2(sqrt(pow(cartesian.z, 2.)
+                                           + pow(cartesian.x, 2.)), cartesian.y) / PI;
 
-      ret.phi = 180 * atan2(cartesian.z, cartesian.x) / PI;
-      return ReduceAngles(ret);
+        ret.phi = 180 * atan2(cartesian.z, cartesian.x) / PI;
+        return ReduceAngles(ret);
     }
 
     Spherical ReduceAngles(const Spherical& in) {
-      Spherical ret = in;
-      double reduce_azimuth = in.phi - 360 * ((int)in.phi / 360);
-      double reduce_elevator = in.theta - 360 * ((int)in.theta / 360);
-      ret.phi = (reduce_azimuth < 0) ? 360 + reduce_azimuth : reduce_azimuth;
-      ret.theta = (reduce_elevator < 0) ? 360 + reduce_elevator : reduce_elevator;
-      return ret;
+        Spherical ret = in;
+        double reduce_azimuth = in.phi - 360 * ((int)in.phi / 360);
+        double reduce_elevator = in.theta - 360 * ((int)in.theta / 360);
+        ret.phi = (reduce_azimuth < 0) ? 360 + reduce_azimuth : reduce_azimuth;
+        ret.theta = (reduce_elevator < 0) ? 360 + reduce_elevator : reduce_elevator;
+        return ret;
     }
 
     Spherical ReducePositiveAngles(const Spherical& in) {
-      Spherical ret = ReduceAngles(in);
-      ret.theta = (ret.theta > 180) ? ret.theta - 360 : ret.theta;
-      ret.phi = (ret.phi > 180) ? ret.phi - 360 : ret.phi;
-      return ret;
+        Spherical ret = ReduceAngles(in);
+        ret.theta = (ret.theta > 180) ? ret.theta - 360 : ret.theta;
+        ret.phi = (ret.phi > 180) ? ret.phi - 360 : ret.phi;
+        return ret;
     }
 }
