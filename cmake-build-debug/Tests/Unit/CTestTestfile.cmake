0d390f29c6891e7f214f86d59063e869d52ed630
Dmitry Ermakov <dmitry.e.ermakov@yandex.ru>
2022-04-29 15:25:51 +0300

    Added gitignore

--- cmake-build-debug/Tests/Unit/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Dmitry/CppWs/Diploma/Tests/Unit
-# Build directory: C:/Users/Dmitry/CppWs/Diploma/cmake-build-debug/Tests/Unit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
